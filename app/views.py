66634d780704f2a8118bbd12771625a4acbdd121
Amine SGHIR <amine.sghir@external.engie.com>
2022-05-30 19:13:26 +0200

    update

--- app/views.py
+++ app/views.py
@@ -4,4 +4,20 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    hello = [
+        { "character": "h", "result": "correct" },
+        { "character": "e", "result": "wrong" },
+        { "character": "l", "result": "close" },
+        { "character": "l", "result": "wrong" },
+        { "character": "o", "result": "close" },
+    ]
+    
+    world = [
+        { "character": "w", "result": "wrong" },
+        { "character": "o", "result": "wrong" },
+        { "character": "r", "result": "wrong" },
+        { "character": "l", "result": "close" },
+        { "character": "d", "result": "correct" },
+    ]
+    
+    return render(request, 'home.html', context={"words": [hello, world]})
