80a82e28636432e46a9b81fb1d5b86f7fb3ccea0
Amine SGHIR <amine.sghir@external.engie.com>
2022-06-01 11:13:11 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # if venv doesn't exist, create it
 if [ ! -d "venv" ]; then
-    ~/.pyenv/versions/3.9.13/bin/python -m venv .venv
+    /root/.pyenv/versions/3.9.13/bin/python -m venv .venv
 fi
 
 .venv/bin/pip install -r requirements.txt
