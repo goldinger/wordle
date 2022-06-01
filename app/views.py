2593de7c0f1ed14e6648d6c33c111ed0c4aebafb
Amine SGHIR <amine.sghir@external.engie.com>
2022-06-01 20:40:19 +0200

    fix ip

--- app/views.py
+++ app/views.py
@@ -17,11 +17,13 @@ def check_word(word, reference) -> Generator:
 
 def home(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    print(x_forwarded_for)
     if x_forwarded_for:
         ip = x_forwarded_for.split(',')[0]
     else:
         ip = request.META.get('REMOTE_ADDR')
-    return ip
+        
+    print(ip)
     current_round = Round.get_current_round()
     goal = current_round.word
     
