3ed3d2bd7912830c544c48c54a76322b7c362fc3
Amine SGHIR <amine.sghir@external.engie.com>
2022-06-01 11:35:25 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@
 if [ ! -d ".venv" ]; then
     python3 -m venv .venv
 fi
-    source .venv/bin/activate
+    . .venv/bin/activate
     # pip install -r requirements.txt
     # python manage.py migrate
