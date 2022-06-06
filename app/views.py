fe233ca6d5787a3c86f7a75bc4a409342bdb38b3
Amine SGHIR <amine.sghir@external.engie.com>
2022-06-06 21:55:07 +0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -47,6 +47,8 @@ def home(request):
             response_data["won"] = True
         elif len(history) >= 5:
             response_data["lost"] = True
+        
+        return render(request, 'home.html', response_data)
     
     elif request.method == 'POST':
         word = request.POST.get("word", '').lower()
@@ -59,29 +61,33 @@ def home(request):
         if goal in history:
             response_data["error"] = "Arrête de spam, t'as gagné..."
             response_data["won"] = True
+            return render(request, 'home.html', response_data)
         elif len(word) != 5:
             response_data["error"] = 'Il faut 5 lettres abruti.e !'
+            return render(request, 'home.html', response_data)
         elif word in history:
             response_data["error"] = 'Déjà essayé !'
+            return render(request, 'home.html', response_data)
         elif len(history) >= 5:
             response_data["error"] = "Abandonne frérot.e, tu est nul.le !"
             response_data["lost"] = True
-        elif len(history) == 4 and word != goal:
+            return render(request, 'home.html', response_data)
+        
+        if word == goal:
+            response_data["won"] = True
+            #don't return request, we need to register the guess
+        elif len(history) == 4:
             response_data["error"] = "Oh mais quel loser.e"
             response_data["lost"] = True
-        else:
-            if word == goal:
-                response_data["won"] = True
-            with open('static/data/words.json', 'r') as f:
-                possible_words = json.load(f)
-            possible_words = [x.lower() for x in possible_words]
-            # print(len(possible_words))
-            if word not in possible_words:
-                response_data["error"] = "Ca n'existe pas enculé.e !"
-                return render(request, 'home.html', response_data)
-            
-            all_words.append(check_word(word, goal))
-            Guess.objects.create(word=word, ip_address=ip, round=current_round)
         
-    return render(request, 'home.html', response_data)
-    
+        with open('static/data/words.json', 'r') as f:
+            possible_words = json.load(f)
+        possible_words = [x.lower() for x in possible_words]
+        # print(len(possible_words))
+        if word not in possible_words:
+            response_data["error"] = "Ca n'existe pas enculé.e !"
+            return render(request, 'home.html', response_data)
+        
+        all_words.append(check_word(word, goal))
+        Guess.objects.create(word=word, ip_address=ip, round=current_round)
+        return render(request, 'home.html', response_data)
